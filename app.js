d905ed891350403ef126e2c32191398ee7300c6a
Matthew Salsamendi <matt@mcprohosting.com>
2014-12-03 17:22:12 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var fs   = require('fs');
+var path = require('path');
+
+var config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config/config.js')));
+
+var ListReader		= require('./lib/listreader')
+var Sender 			= require('./lib/sender');
+var listReader 		= new ListReader(config);
+
+
+listReader.readEmailList(function(list) {
+	var sender = new Sender(config, list);
+	sender.send(function(emailsSent, executionTime) {
+		setTimeout(function() {
+			console.log("E-Mail sending complete! Sent a total of " + emailsSent + " emails over the course of " + executionTime + " seconds.");
+			process.exit();
+		}, 1500);
+	});
+});
---
+++ config/config.example.js
@@ -0,0 +1,31 @@
+{
+	"readonly": false,
+
+	"email": {
+		"templatePath": "templates/example.ejs",
+		"from": "MCProHosting <noreply@mcprohosting.com>",
+		"subject": "This is a test",
+		"marketing": false
+	},
+
+	"throttle": {
+		"interval": 1
+	},
+
+	"smtp": {
+		"host": "127.0.0.1",
+		"port": 587,
+		"secure": false,
+		"auth": {
+			"user": "",
+			"pass": ""
+		}
+	},
+
+	"db": {
+		"host": "",
+		"user": "",
+		"password": "",
+		"database": ""
+	}
+}
