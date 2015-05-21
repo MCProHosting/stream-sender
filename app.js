e79bcfa8f44d7afb0450a43a924ca2463425fefc
Mark Dietzer <mriq91@gmail.com>
2015-05-21 17:44:51 +0200

    Dynamic reader

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ var StaticReader     = require('./lib/staticreader')
 var Sender              = require('./lib/sender');
 var config              = require('./config/config.js');
 
-//var listReader = new WHMCSListReader(config);
-var listReader = new StaticReader(config);
+var listReader = require('./lib/' + config.reader)(config);
 
 listReader.readEmailList(function(list) {
     var sender = new Sender(config, list);
